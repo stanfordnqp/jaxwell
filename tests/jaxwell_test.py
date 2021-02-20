62f02297bbbc59419c58d0e1233f985f21ea0955
Jesse Lu <jesselu@hollisterphotonics.com>
2021-02-19 20:56:22 -0800

    put in code for the adjoint solve

--- tests/jaxwell_test.py
+++ tests/jaxwell_test.py
@@ -26,6 +26,19 @@ class TestJaxwell(unittest.TestCase):
     self.assertEqual(len(errs), 1)
     self.assertAlmostEqual(errs[0], 35.25115523)
 
+  def test_adjoint(self):
+    b = onp.zeros((1, 1, 10, 10, 10), onp.complex128)
+    b[0, 0, 5, 5, 5] = 1.
+    b = vecfield.VecField(0 * b, 0 * b, b)
+    z = vecfield.zeros((1, 1, 10, 10, 10))
+    x, errs = jaxwell.solve(z,
+                            b,
+                            ths=((10, 10), ) * 3,
+                            pml_params=operators.PmlParams(w_eff=0.3),
+                            max_iters=1,
+                            adjoint=True)
+    self.assertAlmostEqual(errs[0], 0.01358992)
+
 
 if __name__ == '__main__':
   unittest.main()
